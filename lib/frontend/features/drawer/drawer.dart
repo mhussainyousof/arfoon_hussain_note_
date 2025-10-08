5ef370acc3a5eefad1f5e37fe4d20fe45805b966
Hussain Yousof <mhussainyousof@gmail.com>
2025-10-08 23:05:24 +0330

    ..

--- lib/frontend/features/drawer/drawer.dart
+++ lib/frontend/features/drawer/drawer.dart
@@ -4,7 +4,6 @@ import 'package:arfoon_note/frontend/theme/responsive.dart';
 import 'package:arfoon_note/integration/integration.dart';
 import 'package:arfoon_note/server/server.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_locales/flutter_locales.dart';
 import 'package:flutter_svg/svg.dart';
 
